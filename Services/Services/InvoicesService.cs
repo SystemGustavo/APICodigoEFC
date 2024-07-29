928d43db2be570c72669f53976bc849073ab311b
CESAR <CESAR@SYSTEMGUSTAVO>
2024-07-28 20:11:36 -0500

    Inclui Referencias y Inyeccion de Dependencia al Contexto al Contructor InvoicesService

--- Services/Services/InvoicesService.cs
+++ Services/Services/InvoicesService.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Infraestructure.Context;
+using Domain.Models;
+using Infraestructure.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace Services.Services
 {
     public class InvoicesService
     {
+        private readonly CodigoContext _context;
+        public InvoicesService(CodigoContext context)
+        {
+            _context = context;
+        }
     }
 }
